9dadd991f118bd351e3e197b94fb23d30d653be1
HoriThe3rd <horitti@gmail.com>
2020-02-08 20:48:52 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,53 @@
+Python environment setup script
+=====
+
+## Overview
+This script sets up below:
+
+- Pyenv
+- Pyenv-virtualenv
+- Create new virtual environment
+- Python 3.6.5
+- python packages: ex) numpy, pandas, etc...
+
+## 概要
+このスクリプトはLinuxにPythonによる機械学習環境(chainer, OpenCV)を構築するためのスクリプトである．具体的には以下の操作が行われる．
+
+なお，Windows Subsystem for Linux (WSL) でも動作の確認中．環境によっては必要なパッケージをaptで要インストール．
+
+- pyenv + pyenv_virtualenvのインストールとセットアップ
+- python3.6.5のインストール
+- python3.6.5におけるchainer用環境をvirtualenvで構築
+- jupyterを入れるのでVSCodeのPython拡張機能があればJupyter Notebook形式でPythonを扱える
+- 構築した環境を全体に適用
+- pipで必要となりそうなパッケージをインストール
+
+## インストールパッケージ一覧
+- numpy
+- six
+- matplotlib
+- pandas
+- opencv-python
+- opencv-contrib-python
+- Pillow
+- jupyter
+- 上記に依存するパッケージ（pipで自動的にインストールされる)
+
+## 使いかた
+このリポジトリをダウンロードし，setup_env.shを実行する．
+pyenvで環境が構築されるので，その環境につけたい名前を引数に与える．
+
+ユーザ権限で実行すること．sudoはつけない．
+
+```sh
+$ ./chainer_env_setup.sh your_env_name
+$ source ~/.bashrc
+```
+これで全て必要な環境が準備される．確認は，pythonと端末に入力し，python3.6.5が起動すればOK．
+
+## おことわり
+本スクリプトはエラー処理を行っていない．何らかの問題でエラー終了した場合は，~/.pyenvを削除し，~/.bashrcをもとに戻す必要がある．
+このスクリプトで生じた事象の一切の責任を作者はおいませんので，自己責任でご利用ください．
+
+## 他のpythonバージョンがほしいとき
+スクリプト内の"PYTHON_VERSION"を所望のバージョンに変更すれば良い．ただし，pyenvでインストールできるバージョンに限る．
---
+++ setup_env.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Check the argument.
+if [ $# -ne 1 ]; then
+    echo "Invalid argument. Please give me a new python environment name you want to create."
+    exit 1
+fi
+
+echo "> Start chainer_env_setup script ..."
+# Please set the python version which you want to install.
+PYTHON_VERSION=3.6.5
+
+# Set the given python environment name.
+PYENV_NAME=$1
+
+echo "> Clone pyenv git repository ..."
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+# Setup arguments.
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+echo "> Setup .bashrc ..."
+echo '# pyenv settings' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+echo "> Installing pyenv-virtualenv ..."
+git clone http://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+echo "> Installing Python ${PYTHON_VERSION} ..."
+pyenv install $PYTHON_VERSION
+
+echo "> Create virtual environment ..."
+pyenv virtualenv $PYTHON_VERSION $PYENV_NAME
+
+echo "> Apply the environment to your global environment ..."
+pyenv global $PYENV_NAME
+
+echo "> Installing python packages ..."
+pip install numpy six matplotlib pandas opencv-python opencv-contrib-python Pillow jupyter
+
+echo -e "\nScript finished.\n###  PLEASE RESTART YOUR TERMINAL.  ###"
